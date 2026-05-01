effd23fd0e89f8e26017d500d5062332bd54bb0c
Bagas Mustoffa Althaf <bagasosh17@gmail.com>
2026-05-01 20:08:51 +0700

    feat : adjustment API-URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://shrewdly-maximize-showgirl.ngrok-free.dev/api';
 
 const bgGreen = '#2e4d37';
 const bgLight = '#fdfefd';
